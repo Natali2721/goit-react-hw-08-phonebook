6cb6406c72aac037d1ba8d1419ec7b10bb1196ec
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-01-04 10:43:36 +0200

    Update Contacts.js

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,17 +1,21 @@
-import { ContactsContainer } from 'components/Style/Element.styled';
+import { Box, ContactsContainer } from 'components/Style/Element.styled';
 import { Filter } from 'components/Filter/Filter';
-import { H1, H2 } from 'components/Title/Title';
+import { H1 } from 'components/Title/Title';
 import { ContactList } from 'components/ContactList/ContactList';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 
 const Contacts = () => {
   return (
     <ContactsContainer>
-      <H1 title="Phonebook" />
-      <ContactForm />
-      <H2 title="Contacts" />
-      <Filter title="Find contact by name" />
-      <ContactList />
+      <Box>
+        <H1 title="Phonebook" />
+        <ContactForm />
+      </Box>
+      <Box>
+        <H1 title="Contacts" />
+        <Filter title="Find contact by name" />
+        <ContactList />
+      </Box>
     </ContactsContainer>
   );
 };
